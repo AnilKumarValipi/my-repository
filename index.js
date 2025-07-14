717500dce54aa93dd310622ccbdbd49b5462e413
AnilKumarValipi <anilv808@gmail.com>
2025-07-14 12:07:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
   res.end("Hello from Node.js on EC2!");
 });
 
-server.listen(3000, "0.0.0.0"), () => {
+server.listen(3000, "0.0.0.0", () => {
   console.log("Server running on port 3000");
 });
